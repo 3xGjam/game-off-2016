9ba4e13e88b477e272e47b331de83764458ebfd8
Jon Sousa <jonathonsousa@gmail.com>
2016-11-05 15:01:28 -0500

    Added the player controller script.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    [HideInInspector] public int score;
+    [HideInInspector] public int winningScore;
+    public Text Score;
+    public Text finalScoreText;
+    public Image GreyScreen;
+
+    public Text gameOverText;               //Text that appears when a player is hit with 3 avoidable objects
+    public Image gameOverPanel;             //Panel that appears when a player is hit with 3 avoidable objects
+    public string loseByAvoidables;         //String value (determined in the Editor) alerting the player that they lost because they were hit by 3 avoidable objects
+    public string TimeRanOut;               //String value (determined in the Editor) alerting the player that they lost because they ran out of time and did not get enough points
+
+    public float timeCounter = 60;          //It's a float so I can subtract it from Time.deltaTime
+    public int bonusTime;                   //used to add time to the clock and adds to your final score
+    public Text timeText;
+
+    public Button replay;
+    public Button btnPause;
+    public Button btnResume;
+    public Button btnLevelSelector;
+    public Button btnHatchery;
+    public Button btnQuit;
+
+    public AudioSource _audioSource;
+    public AudioClip normalMusic;
+    public AudioClip gameOverMusic;
+
+    public bool pause;
+    public bool gameOver;
+    
+
+    void Awake()
+    {
+        if (gameManager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameManager = this;
+        }
+        else if (gameManager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        gameOverText.text = "";
+        finalScoreText.text = "";
+        Score.text = "";
+        score = 0;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    GameManager gameManager;
+
+    public GameObject[] hackySacks;             //objects the player wants to hit him
+    public Vector3 spawnValues;                 //Allows you to modify the hazard's position when it is instantiated in the unity editor.
+    public GameObject[] avoidables;             //DODGE!
+
+    void Start()
+    {
+        gameManager = GetComponent<GameManager>();
+
+    }
+
+    /*
+    IEnumerator SpawnFood()
+    {
+        fruitRunning = true;   //In FixedUpdate, "turns off" the coroutine and stops the FixedUpdate from creating any more instances.
+
+        yield return new WaitForSeconds(startWait);
+        while (gameOver != true && pause != true)
+        {
+            for (int i = 0; i <= foodCount; i++)
+            {
+                GameObject food = Foodstuffs[Random.Range(0, Foodstuffs.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);         //inserts the values from the unity editor into the hazard's spawnPosition.
+                Quaternion spawnRotation = Quaternion.identity;     //Means our food will have no new rotation.
+                Instantiate(food, spawnPosition, spawnRotation);
+                if (pause == true || gameOver == true)
+                { break; }                              //Breaks the loop so fruit will stop spawning in (it finishes it's loop before stopping) DO NOT USE YIELD BREAK UNLESS YOU WANT THE LOOP TO STAY BROKEN UNTIL YOU START THE SCRIPT AGAIN!!!
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waiveWait);
+        }
+
+        fruitRunning = false;  //In FixedUpdate, one SpawnFood Coroutine is allowed to pass.
+    }
+
+    IEnumerator SpawnAvoidables()
+    {
+        avoidableRunning = true;   //In FixedUpdate, "turns off" the coroutine and stops the FixedUpdate from creating any more instances.
+
+        yield return new WaitForSeconds(avoidStartWait);
+        while (gameOver != true && pause != true)
+        {
+            for (int i = 0; i <= avoidCount; i++)
+            {
+                GameObject avoidable = Avoidables[Random.Range(0, Avoidables.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);         //inserts the values from the unity editor into the hazard's spawnPosition.
+                Quaternion spawnRotation = Quaternion.identity;     //Means our food will have no new rotation.
+                Instantiate(avoidable, spawnPosition, spawnRotation);
+                if (pause == true || gameOver == true)
+                { break; }                                  //Breaks the loop so fruit will stop spawning in (it finishes it's loop before stopping) DO NOT USE YIELD BREAK UNLESS YOU WANT THE LOOP TO STAY BROKEN UNTIL YOU START THE SCRIPT AGAIN!!!
+                yield return new WaitForSeconds(avoidSpawnWait);
+            }
+            yield return new WaitForSeconds(avoidWaiveWait);
+        }
+
+        avoidableRunning = false;  //In FixedUpdate, one SpawnFood Coroutine is allowed to pass.
+    }
+    */
+}
